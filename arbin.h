318fad6a2b06f03cc2fe6345f398806fd63fabd3
uuulises <134988248+uuulises@users.noreply.github.com>
2023-05-30 16:29:13 -0300

    Update arbin.h

--- arbin.h
+++ arbin.h
@@ -1,6 +1,6 @@
-#ifndef ARBOLBINARIO_H
-#define ARBOLBINARIO_H
-//#include "LISTA.h"
+#ifndef ARBIN_H
+#define ARBIN_H
+#include "Lista.h"
 
 
 template <typename T>
@@ -15,12 +15,13 @@ private:
     };
     nodo *raiz;
     // estas son las fucniones que use para los distintos procesos recursivos de las funciones de la clase(not4 public)
-    void insertarordenado(nodo *raiz, nodo *aux);
+    void insertarordenado(nodo*& raiz, nodo *aux);
     int profundidadarbol(nodo *raiz) const;
     int cantidadnodos(nodo *raiz) const;
     void vaciar(nodo *raiz);
     bool buscarcoincidenciaenarbol(nodo *raiz, const T & elemento) const;
-
+    Lista<T> inorder(Lista<T> & G, nodo*raiz);
+    Lista<T> listarhojas(Lista<T> & F, nodo*raiz);
 
 public:
 
@@ -32,49 +33,12 @@ public:
     int cantidadnodosarbol() const;
     bool esvacio() const;
     int profundidad() const;
-    const T & listarelementosultimahoja() const; //se refiere a devolver una lista con los elementos de la ultima hoja
-    const T & listarelementosordenados() const;
+    Lista<T> listarordenado();
+    Lista<T> frontera();
+
 
 
 };
 
-#endif // ARBOLBINARIO_H
+#endif
 
-
-//// Esto es lo que hizo vico para el tema de las listas
-
-/*#ifndef ARBIN_H
-#define ARBIN_H
-#include "LISTA.h"
-
-
-template <typename T>
-class ARBIN
-{
-    public:
-        ARBIN();
-        virtual ~ARBIN();
-        void agregar(const T & elemento);
-        bool pertenece(const T &elemento);
-        int cantidadnodos() const;
-        bool vacio() const;
-        int profundidad() const;
-        void frontera(LISTA<T> & lista);
-        LISTA<T> inorder();
-
-    protected:
-
-    private:
-        struct Nodo{
-            T elemento;
-            ARBIN * mayor;
-            ARBIN * menor;
-        };
-        void fronteraPriv(LISTA<T>&lista);
-
-        void inOrderPriv(LISTA<T>lista);
-
-        Nodo * raiz;
-
-        void vaciar();
-};*/
